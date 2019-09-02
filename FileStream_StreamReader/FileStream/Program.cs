a687a49715e8159b24e08bafa43915f7ff93f26e
Gabriela Vilela <gabrielathalitasv@gmail.com>
2019-09-02 13:43:31 -0300

    Aprendendo FileStream e StreamReader

---
+++ FileStream_StreamReader/FileStream/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+           
+            StreamReader sr = null;
+
+            try
+            {
+               
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream) { 
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("Um erro ocorreu!");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+             
+                if (sr != null) sr.Close();
+            }
+
+        }
+    }
+}
