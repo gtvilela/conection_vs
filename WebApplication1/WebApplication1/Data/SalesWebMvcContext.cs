dda9c629bcef3add1fd6f203a27ac0bbfe2dde03
Gabriela Vilela <gabrielathalitasv@gmail.com>
2019-09-24 14:27:25 -0300

    Resultado do Scaffolding

---
+++ WebApplication1/WebApplication1/Data/SalesWebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Department> Department { get; set; }
+    }
+}
